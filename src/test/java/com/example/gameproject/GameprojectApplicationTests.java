06739d6379fade11a2d1bc4dfd5f4e4dddb82370
lllillly <43092333+lllillly@users.noreply.github.com>
2023-10-23 12:50:08 +0900

    settings

---
+++ src/test/java/com/example/gameproject/GameprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
