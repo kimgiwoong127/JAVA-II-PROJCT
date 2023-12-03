ecec1fc3035b99d89f38f22d5b123119c4ed9c25
[github <[github>
2023-12-04 04:27:38 +0900

    ABC

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -12,11 +12,11 @@ public class Player {
         Idle idleCharacter = new Idle("image\\Player\\1 Pink_Monster\\Pink_Monster_Idle\\Pink_Monster_idle_");
 
         // Set the bounds of the idleCharacter within playerPanel
-        idleCharacter.setBounds(0, 120, 80, 80); // 좌표를 (0, 0)으로 변경
+        idleCharacter.setBounds(0, 288, 80, 80); // 좌표를 (0, 0)으로 변경
 
         // Create a JPanel to contain the player components
         playerPanel = new JPanel();
-        playerPanel.setSize(80, 80);
+        playerPanel.setSize(688, 400);
         playerPanel.setOpaque(false); // 배경을 투명하게 설정
         playerPanel.setLayout(null);
         playerPanel.add(idleCharacter);
