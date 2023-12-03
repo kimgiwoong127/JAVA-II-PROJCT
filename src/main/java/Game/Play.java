b1626a36fbcfc1e70c8b165f1af459f3e04ca1a6
[github <[github>
2023-12-04 03:26:10 +0900

    Slice

--- src/main/java/Game/Play.java
+++ src/main/java/Game/Play.java
@@ -1,8 +1,8 @@
 package Game;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+
 import Engine.MapEditor.CombinedPanel;
 
 
@@ -11,13 +11,11 @@ public class Play extends JFrame {
         CombinedPanel combinedPanel = new CombinedPanel();
         combinedPanel.setSize(1366, 760);
 
-        JButton sampleButton = new JButton("클릭하세요");
-        combinedPanel.add(sampleButton);
-
-        setTitle("이어하기");
+        setTitle("forest1");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setContentPane(combinedPanel);
 
+        setSize(688, 400);
         setLocationRelativeTo(null);
         setVisible(true);
     }
